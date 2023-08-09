a542cb71d38057c0a3656d7d052a4364301a24a6
서민재 <>
2023-08-09 15:20:50 +0900

    feat : Item17 예제 추가

--- EffectiveJavaStudy/Chapter4/DevSeoRex/Item17/BigTest.java
+++ EffectiveJavaStudy/Chapter4/DevSeoRex/Item17/BigTest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class BigTest {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class BigTest {
+
+    public static void main(String[] args) {
+
+        BigDecimal bigDecimal = new BigDecimal(10);
+        BigDecimal bigDecimal2 = bigDecimal.add(BigDecimal.valueOf(10));
+
+        BigInteger bigInteger = new BigInteger("10");
+        BigInteger bigInteger2 = bigInteger.add(BigInteger.valueOf(20));
+
+
+
+        System.out.println(bigDecimal.equals(bigDecimal2)); // false
+        System.out.println(bigDecimal.compareTo(bigDecimal2)); // -1
+        System.out.println((bigDecimal == bigDecimal2)); // false
+
+        System.out.println(bigInteger.equals(bigInteger2)); // false
+        System.out.println(bigInteger.compareTo(bigInteger2)); // -1
+        System.out.println((bigInteger == bigInteger2)); // false
+    }
 }
