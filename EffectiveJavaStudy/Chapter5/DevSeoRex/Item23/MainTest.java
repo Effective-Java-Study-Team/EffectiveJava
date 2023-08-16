5ffb1503e6b3de7d8bb4f40b777600299c4bfcfa
서민재 <seominjae@seominjaeui-Macmini.local>
2023-08-16 22:50:57 +0900

    feat : Item23 예제 추가

---
+++ EffectiveJavaStudy/Chapter5/DevSeoRex/Item23/MainTest.java
@@ -0,0 +1,14 @@
+public class MainTest {
+
+    public static void main(String[] args) {
+        Figure circle = new Figure(2.3);
+        Figure rectangle = new Figure(2.2, 3.3);
+
+
+        Rectangle a = new Rectangle(2, 3.3);
+        Circle b = new Circle(2.4);
+        AbstractFigure circle2 = new Circle(2.4);
+        AbstractFigure rectangle2 = new Rectangle(2.6, 2.8);
+
+    }
+}
